faf45c47cfbb41f535ce4a451d738d329ff114df
Alexander Ong <FireFox2000000@gmail.com>
2018-01-27 11:36:46 +1100

    Basic enemy sniper AI and updated collision matrix.

--- Assets/Scripts/EnemySniper.cs
+++ Assets/Scripts/EnemySniper.cs
@@ -5,16 +5,24 @@ using UnityEngine;
 public class EnemySniper : Sniper {
     [SerializeField]
     float m_timeTillShotIsFired = 5;
+    [SerializeField]
+    UnityEngine.UI.Text countdownText;
 
     AiMessenger m_target = null;
     float m_shotCountdownTimer;
+    Renderer m_renderer;
+
+    Quaternion m_startRotation, m_endRotation;
+    float m_rotationLerpTimer = 0;
 
 	// Use this for initialization
 	protected override void Start () {
         base.Start();
+        m_renderer = GetComponent<Renderer>();
 
-        gameObject.SetActive(false);    // Enemies are disabled until they are activated by a trigger
-	}
+        m_startRotation = transform.rotation;
+        m_endRotation = m_startRotation;
+    }
 	
 	// Update is called once per frame
 	protected override void Update () {
@@ -22,17 +30,38 @@ public class EnemySniper : Sniper {
 
         if (m_target && m_target.gameObject.activeSelf)
         {
+            Vector3 directionToTarget = (m_target.transform.position - transform.position).normalized;
             m_timeTillShotIsFired -= Time.deltaTime;
 
+            m_endRotation = Quaternion.LookRotation(directionToTarget);
+
+            // Rotate the model to face the target
+            transform.rotation = Quaternion.Lerp(m_startRotation, m_endRotation, m_rotationLerpTimer);// Quaternion.LookRotation(directionToTarget);
+
+            // Enough time has elapsed, fire at the target
             if (m_timeTillShotIsFired <= 0)
             {
-                Vector3 shotDirection = transform.position - m_target.transform.position;
+                Vector3 shotDirection = directionToTarget;
                 Ray ray = new Ray(transform.position, shotDirection);
+                Shoot(ray);
             }
+
+            m_rotationLerpTimer += Time.deltaTime;
         }
         else
         {
+          // Lost the target
             ResetTimer();
+        }
+
+        if (countdownText)
+        {
+            countdownText.gameObject.SetActive(m_target);
+
+            float time = m_timeTillShotIsFired;
+            if (time < 0)
+                time = 0;
+            countdownText.text = string.Format("{0:f2}", time);
         }
 	}
 
@@ -45,5 +74,15 @@ public class EnemySniper : Sniper {
     void ResetTimer()
     {
         m_shotCountdownTimer = m_timeTillShotIsFired;
+    }
+
+    void OnTriggerEnter(Collider coll)
+    {
+        AiMessenger messenger = coll.gameObject.GetComponent<AiMessenger>();
+
+        if (!m_target && messenger)
+        {
+            m_target = messenger;
+        }
     }
 }
