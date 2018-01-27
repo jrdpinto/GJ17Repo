a1cf4f2f61db270f9282bf2aa6fbe334ec95b9f0
Alexander Ong <FireFox2000000@gmail.com>
2018-01-28 09:29:22 +1100

    Main level work + added disabler trigger to disable enemy snipers that fall off screen.

---
+++ Assets/Scripts/ObjectActiveToggleTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectActiveToggleTrigger : MonoBehaviour {
+    public GameObject[] m_objects;
+    public bool m_enableOnTriggerEnter = false;
+    public string tagTrigger = "Player";
+
+    void OnTriggerEnter (Collider coll)
+    {
+        if (coll.tag == tagTrigger)
+        {
+            foreach(GameObject obj in m_objects)
+            {
+                obj.SetActive(m_enableOnTriggerEnter);
+            }
+        }
+    }
+}
