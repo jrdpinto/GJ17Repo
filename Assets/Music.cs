62eb66fbc0c35a5dada19c0f5b1cc4c1bd1eeb8f
Josh <joshua_laurence@live.com>
2018-01-27 17:13:12 +1100

    All audio (maybe so far) and some scripts for music and restarting the level.

---
+++ Assets/Music.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class Music : MonoBehaviour {
+
+    new AudioSource audio;
+    public AudioClip musicStart;
+	public AudioClip musicLoop;
+
+
+    private static Music instance = null;
+    public static Music Instance
+    {
+        get { return instance; }
+    }
+    void Awake()
+    {
+        if (instance != null && instance != this) {
+            Destroy(this.gameObject);
+            return;
+        } else {
+            instance = this;
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    void Start()
+	{
+        audio = GetComponent<AudioSource> ();
+        audio.loop = true;
+
+        StartCoroutine(playEngineSound());
+	}
+
+	IEnumerator playEngineSound()
+	{
+		audio.clip = musicStart;
+		audio.Play();
+		yield return new WaitForSeconds(audio.clip.length);
+		audio.clip = musicLoop;
+		audio.Play();
+	}
+}
