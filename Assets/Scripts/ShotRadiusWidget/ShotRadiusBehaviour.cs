6b30176830ba89ad52b3f5b14346a90457d628b3
jpinto <abcd@efgh>
2018-01-30 21:36:37 +1100

    Fixed tabs.

--- Assets/Scripts/ShotRadiusWidget/ShotRadiusBehaviour.cs
+++ Assets/Scripts/ShotRadiusWidget/ShotRadiusBehaviour.cs
@@ -5,10 +5,9 @@ using UnityEngine;
 public class ShotRadiusBehaviour : MonoBehaviour {
 
     public GameObject messenger_;
-	
-	// Update is called once per frame
-	void FixedUpdate()
-    {
-            transform.position = messenger_.transform.position;
+    
+    // Update is called once per frame
+    void FixedUpdate() {
+        transform.position = messenger_.transform.position;
     }
 }
